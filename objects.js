0ae734e72b6ae1628aea6e1cb8ddcf444e3a84c2
candescent-mouse-0917 <aqiph818@gmail.com>
2020-02-06 20:31:45 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,13 @@
+let playlist = {Jay: ''};
+
+//updatePlaylist
+function updatePlaylist(playlist, artist, song){
+  playlist[artist] = song;
+  return playlist;
+}
+
+//removeFromPlaylist
+function removeFromPlaylist(playlist, artist){
+  delete playlist[artist];
+  return playlist;
+}
